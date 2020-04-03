8803eb361a6ba0d5b5046371a02f3a42bb6768a0
Maria Lagunero <maria58@gmail.com>
2020-04-03 15:30:07 -0700

    add more specs to readme

--- README.md
+++ README.md
@@ -22,10 +22,17 @@ _-Example Output: Sorry, not a number!_
 -Example Output: "0, 1, 2, 3, 4"
 
 * _Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"_
+_Example Input: 11
+-Example Output: "Beep"
+
 
 * _Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"_
+_Example Input: 21
+-Example Output: "Boop"_
 
 * _Numbers that contain a 3: all digits are replaced (all digits) with "Won't you be my neighbor?"_
+_-Example Input: 13
+-Example Output: "Won't you be my neighbor?"_
 
 ## **Support and Contact Details**
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 //business logic
+//1. userInput that is nonNumber will get "alert" 
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//var nonNumber= use regex?
+
+//2. userInput that is number will get "alert"
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 numbers.forEach(function(number){
   alert(number)
 
@@ -10,8 +15,8 @@ numbers.forEach(function(number){
 
 //split? slice? includes? forEach? map? includes? push? join?
 
-//1. userInput that is nonNumber will get "alert" 
-//2. userInput that is  
+
+
 
 
 
@@ -23,7 +28,7 @@ $(document).ready(function() {
     event.preventDefault();
 
     var numbers = parseInt($("numberInput").val())
-      //var result = phrase(number)
+      var result = phrase(number)
   
     
   });
