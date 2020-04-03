25a78e550422027ec39d4917eca1c2e12d2b673c
Maria Lagunero <maria58@gmail.com>
2020-04-03 14:41:49 -0700

    add alert for first behavior code returns value

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
 //business logic
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var numberValues;
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.forEach(function(number){
+  alert(number)
+
+});
+
 
 
 //split? slice? includes? forEach? map? includes? push? join?
 
-var numbers = function(number) {
-var numberValues = [];
-numbers.forEach(function(number) {
-
-});
-  
-
 //1. userInput that is nonNumber will get "alert" 
 //2. userInput that is  
 
-
-
-};
 
 
 
@@ -28,8 +22,8 @@ $(document).ready(function() {
   $("form#number").submit(function(event) {
     event.preventDefault();
 
-    var numbers = parseInt($("numberInput").val()
-      var result = phrase(number)
+    var numbers = parseInt($("numberInput").val())
+      //var result = phrase(number)
   
     
   });
