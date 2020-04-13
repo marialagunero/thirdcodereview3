14b931252f095ccba42ebf9fa9931e02417eed8c
Maria Lagunero <maria58@gmail.com>
2020-04-12 19:25:59 -0700

    add for loop and methods toString and includes in js still working on code.

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,28 @@
 //if index contains a 1 replace with "Beep" show new Array
 //else if index contains 2 replace with "Boop" show new Array
 //else if index contains 3 replace with "Wont you be my neighbor?" show new Array
-//order logic 3, 2, 1
+//order logic 3, 2, 1 most to least (?)
 //display in UI
 
 var myNumbers = [];
-var translate
-for(let i = 0; i < userInput; i ++)
+var translate = function() {
+  var userInput = $("#numberInput").val();
+  for (i = 0; i <= userInput; i ++) {
+    if(i.toString().includes(3)){
+      myNumbers.push("Won't you be my neighbor")
+    }else if(i.toString().includes(2)){
+      myNumbers.push("Boop")
+    }else if(i.toString().includes(1)){
+      myNumbers.push("Beep")
+    }else{
+      myNumbers.push(i);
+    }
+    }
+
+    return myNumbers
+    }
 
 
-if (myNumber === 1) {
-  alert("beep");
-}else if (myNumber === 2) {
-  alert("boop");
-}else if (myNumber === 3) {
-  alert("won't you be my neighbor");
-}else {alert(myNumber);
-
-};
-});
 
 
 //user interface logic
@@ -29,8 +33,11 @@ $(document).ready(function() {
   $("form#number").submit(function(event) {
     event.preventDefault();
 
-    var numbers = parseInt($("#numberInput").val())
-    //var result = translate(myNumber) 
+    myNumbers = []
+
+    var numbers = parseInt($("#numberInput").val());
+    $("#numberOutput").text(translate().toString());
+    
   
     
   });
